8062c558c919442642a23d99cbc8882f500ee549
hloops <hlcs082@gmail.com>
2023-01-20 15:40:20 +0000

    First MVC - Department

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
-﻿  using Microsoft.AspNetCore.Mvc;
-using SalesWebMVC.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
